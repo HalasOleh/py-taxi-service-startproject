873c519785c19d78cb619bd581fdfcf750cfee7a
Oleh Halas <oleggalas8@gmail.com>
2026-04-24 10:50:48 +0300

    Change Car model related_name

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -20,11 +22,11 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer,
                                      on_delete=models.CASCADE,
-                                     related_name='manufacturer'
+                                     related_name='cars'
                                      )
-    drivers = models.ManyToManyField(Driver,
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                      blank=True,
-                                     related_name='car')
+                                     related_name='cars')
 
     def __str__(self):
         return self.model
